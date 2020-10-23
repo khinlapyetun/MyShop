d7e2ae5b7099e9f5ec5fae92a42d024569df2ade
LP Tun <Lapye28@gmail.com>
2020-10-23 14:22:51 +0630

    Product Grouping adding

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRespository.cs
@@ -0,0 +1,75 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRespository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productcategories;
+
+        public ProductCategoryRespository()
+        {
+            productcategories = cache["productcategories"] as List<ProductCategory>;
+            if (productcategories == null)
+            {
+                productcategories = new List<ProductCategory>();
+            }
+        }
+        public void Commit()
+        {
+            cache["productcategories"] = productcategories;
+        }
+        public void Insert(ProductCategory p)
+        {
+            productcategories.Add(p);
+        }
+        public void Update(ProductCategory productcategory)
+        {
+            ProductCategory productCategoryToUpdate = productcategories.Find(p => p.Id == productcategory.Id);
+            if (productCategoryToUpdate != null)
+            {
+                productCategoryToUpdate = productcategory;
+            }
+            else
+            {
+                throw new Exception("Product Category no found");
+            }
+        }
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory productCategory = productcategories.Find(p => p.Id == Id);
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category no found");
+            }
+        }
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productcategories.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = productcategories.Find(p => p.Id == Id);
+            if (productCategoryToDelete != null)
+            {
+                productcategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
@@ -12,9 +13,12 @@ namespace MyShop.WebUI.Controllers
     {
         // GET: ProductManager
         ProductRepository context;
+        ProductCategoryRespository productCategories;
+
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRespository();
         }
         public ActionResult Index()
         {
@@ -23,8 +27,10 @@ namespace MyShop.WebUI.Controllers
         }
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -51,7 +57,10 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
 
