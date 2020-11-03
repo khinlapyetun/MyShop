70301392e31c444ba2d190177bf67d428951f40e
LP Tun <Lapye28@gmail.com>
2020-11-03 19:45:14 +0630

    Basket Tests

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -13,17 +16,22 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
+
         {
-            // Arrange
-            //HomeController controller = new HomeController();
+            //Setup
+            IRespository<Product> ProductContext = new Mocks.MockContext<Product>();
+            IRespository<ProductCategory> ProductCategoryContext = new Mocks.MockContext<ProductCategory>();
+           
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            //Act
+            ProductContext.Insert(new Product());
+            HomeController controller = new HomeController(ProductContext, ProductCategoryContext);
+            var result = controller.Index() as ViewResult;
+            var viewmodel = (ProductListViewModel)result.ViewData.Model;
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            //Assert
+            Assert.AreEqual(1, viewmodel.Products.Count());
         }
-
     }
 }
