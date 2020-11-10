6f075fae9d5707ffec9906333567ec741acd07cf
LP Tun <Lapye28@gmail.com>
2020-11-10 14:08:05 +0630

    Order manage

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -34,5 +34,19 @@ namespace MyShop.Services
                 OrderContext.Commit();
             }
         }
+        public List<Order> GetOrderList()
+        {
+            return OrderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+            return OrderContext.Find(Id);
+        }
+        public void UpdateOrder(Order updateOrder)
+        {
+            OrderContext.Update(updateOrder);
+            OrderContext.Commit();
+        }
     }
 }
